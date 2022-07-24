a887840c4e3616bbf2f0d3cff33468234e0fb335
sreelekshmimohanan <sreelekshmimohanan1999@gmail.com>
2022-07-24 20:03:46 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css.map'
+import Register from './Component/Register';
+
+
+function App() {
+  return (
+
+  <Register />
+  );
+}
+
+export default App;
---
+++ src/Component/Register.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+        <div class="container">
+    <div class="row">
+        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div class="row g-3">
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Name</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Age</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Course</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                  <button class="btn btn-success">Register</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default Register
